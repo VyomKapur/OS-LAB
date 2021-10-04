f368c4eda0a7fea0483cdab887d816bd436318a2
Vyom <vkapur_be20@thapar.edu>
2021-10-04 10:26:21 +0530

    FCFS finalized

--- SJF.cpp
+++ SJF.cpp
@@ -6,15 +6,41 @@ struct process{
     int tat;
     int wt;
     int pid;
-}
+}p[10];
 int main(){
     int n;
     int time = 0;
-    cout<<"Enter number of processes to be run: ";
-    cin>>n;
+    do{
+        cout<<"Enter number of processes to be run: ";
+        cin>>n;
+    }while (n<10);
     for (int i = 0; i < n; i++)
     {
-        /* code */
+        cout<<"Enter burst time of process "<<i+1<<": ";
+        cin>>p[i].bt;
+        cout<<"Enter arrival time of process "<<i+1<<": ";
+        cin>>p[i].at;
+        p[i].pid = i+1;
     }
-    
+    for(int i=0; i<(n-1); i++)
+    {
+        for(int j=0; j<(n-i-1); j++)
+        {
+            if(p[j].at>p[j+1].at)
+            {
+                struct process temp;
+                temp = p[j];
+                p[j] = p[j+1];
+                p[j+1] = temp;
+            }
+            if(p[j].at==p[j+1].at){
+                if(p[j].pid>p[j+1].pid){
+                    struct process temp;
+                    temp = p[j];
+                    p[j] = p[j+1];
+                    p[j+1] = temp;
+                }
+            }
+        }
+    }
 }
