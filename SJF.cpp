1515820724de1c8ad5c4a535b97073df52a65306
Vyom <vkapur_be20@thapar.edu>
2021-10-04 12:49:19 +0530

    progress

--- SJF.cpp
+++ SJF.cpp
@@ -6,14 +6,14 @@ struct process{
     int tat;
     int wt;
     int pid;
-}p[10];
+}p[10], r[10];
 int main(){
     int n;
     int time = 0;
     do{
         cout<<"Enter number of processes to be run: ";
         cin>>n;
-    }while (n<10);
+    }while (n>10);
     for (int i = 0; i < n; i++)
     {
         cout<<"Enter burst time of process "<<i+1<<": ";
@@ -34,7 +34,7 @@ int main(){
                 p[j+1] = temp;
             }
             if(p[j].at==p[j+1].at){
-                if(p[j].pid>p[j+1].pid){
+                if(p[j].at>p[j+1].at){
                     struct process temp;
                     temp = p[j];
                     p[j] = p[j+1];
@@ -42,5 +42,41 @@ int main(){
                 }
             }
         }
+    }
+    int k = 0;
+    int ctr = n;
+    while (ctr!=0)
+    {
+        for(int i=0;i<n;i++){
+            if(time>=p[i].at){
+                r[k] = p[i];
+            }
+        }
+        k++;
+        for(int i=0; i<(k-1); i++)
+        {
+            for(int j=0; j<(k-i-1); j++)
+            {
+                if(r[j].bt>r[j+1].bt)
+                {
+                    struct process temp;
+                    temp = r[j];
+                    r[j] = r[j+1];
+                    r[j+1] = temp;
+                }
+                if(r[j].bt==r[j+1].bt){
+                    if(r[j].pid>r[j+1].pid){
+                        struct process temp;
+                        temp = r[j];
+                        r[j] = r[j+1];
+                        r[j+1] = temp;
+                    }
+                }
+            }
+        }
+        cout<<"Running process with pid: "<<r[0].pid<<endl;
+        time += r[0].bt;
+        cout<<"Time elasped: "<<time<<endl;
+        ctr--;
     }
 }
